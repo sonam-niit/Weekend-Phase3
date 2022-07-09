d4127480a1352b44fcb91b734faac7ab679a0725
unknown <sonam.gravity@gmail.com>
2022-07-09 23:04:44 +0530

    spring MVC

---
+++ Session-1/springmvc/src/main/java/com/simplilearn/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.simplilearn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/register")
+	public String hanldeRequest() {
+		return "register";
+	}
+}
